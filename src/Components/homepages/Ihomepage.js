a2399f1a2841a5bde5a8e24254fa280d5c25faa4
Lajja123 <lajja.lamprostech@gmail.com>
2023-12-21 10:17:50 +0530

    delete lihomepage.js and add code to lhomepage.js file

--- src/Components/homepages/Ihomepage.js
+++ src/Components/homepages/Ihomepage.js
@@ -1,6 +1,11 @@
 // Ihomepage.js
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import gif from "../../Assets/output-onlinegiftools.gif";
+import historyview from "../../Assets/view.gif";
+import send from "../../Assets/sendgif.gif";
+import list from "../../Assets/listgii.gif";
+import "../../Styles/iihomepage.css";
 import {
   faArrowRight,
   faGlobe,
@@ -90,6 +95,35 @@ function Ihomepage() {
           >
             Get Started <FontAwesomeIcon icon={faArrowRight} />
           </button>
+        </div>
+      </div>
+
+      <div
+        className={`main-div-for-user-guide ${
+          showModal ? "blur-background" : ""
+        }`}
+      >
+        <div className="rectangle-box-for-4-cards">
+          <div id="a" className="card">
+            <img className="iconnn" src={gif} alt="non" />
+            <h3 className="iconn">Connect Your Wallet</h3>
+            <p>Link your Wallet</p>
+          </div>
+          <div id="b" className="card">
+            <img className="iconnn" src={list} alt="non" />
+            <h3 className="iconn">List Transactions</h3>
+            <p>Enter Recipient Details</p>
+          </div>
+          <div id="c" className="card">
+            <img className="iconnn" src={send} alt="non" />
+            <h3 className="iconn">Send Transaction</h3>
+            <p>Initiate the transaction</p>
+          </div>
+          <div id="d" className="card">
+            <img className="iconnn" src={historyview} alt="non" />
+            <h3 className="iconn">View History</h3>
+            <p>Monitor your Transactions</p>
+          </div>
         </div>
       </div>
       {showModal && (
--- src/Components/pages/Landingpage.js
+++ src/Components/pages/Landingpage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../Styles/landingpage.css";
 import Ihomepage from "../homepages/Ihomepage";
-import Iihomepage from "../homepages/Iihomepage";
+// import Iihomepage from "../homepages/Iihomepage";
 import Footer from "../homepages/Footer";
 import img1 from "../../Assets/Illustration (30) 1.png";
 import img2 from "../../Assets/Illustration (30) 2.png";
@@ -15,7 +15,7 @@ function Landingpage() {
       <img className="image-left" src={img2} alt="none" />
       <img className="image-right" src={img1} alt="none" />
       <Ihomepage />
-      <Iihomepage />
+      {/* <Iihomepage /> */}
       {/* <Footer /> */}
     </div>
   );
