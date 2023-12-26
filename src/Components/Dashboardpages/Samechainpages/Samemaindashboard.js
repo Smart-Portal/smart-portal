6e1b1f9bcc038f3a0aef6d5aa1cf5fcd79553473
Lajja123 <lajja.lamprostech@gmail.com>
2023-12-26 14:00:47 +0530

    remove the same container of all buttons

--- src/Components/Dashboardpages/Samechainpages/Samemaindashboard.js
+++ src/Components/Dashboardpages/Samechainpages/Samemaindashboard.js
@@ -57,55 +57,7 @@ function Samemaindashboard() {
               Multiple Accounts in One Click
             </h3>
           </div>
-          <div className="main-div-for-all-option-dashboard">
-            <div className="menu-bar-dashboard">
-              <button
-                id="view"
-                className={activeTab === "text" ? "active" : ""}
-                onClick={() => setActiveTab("text")}
-                data-tip="Create Transaction Text"
-              >
-                {/* <img className="image-option" src={text} alt="none" /> */}
-                {/* Create Transaction Text */}
-                Textify
-              </button>
-              <button
-                id="create"
-                className={activeTab === "create" ? "active" : ""}
-                onClick={() => setActiveTab("create")}
-                data-tip="Create Transaction List"
-              >
-                Listify
-                {/* <img className="image-option" src={list} alt="none" /> */}
-                {/* Create Transaction List */}
-              </button>
-              <button
-                id="csv"
-                className={activeTab === "list" ? "active" : ""}
-                onClick={() => setActiveTab("list")}
-                data-tip="Upload Transaction List"
-              >
-                Uploadify
-                {/* Upload Transaction List */}
-              </button>
-              {/* <button
-        <Navbar />
-        <div style={{ position: "relative" }}>
-          <img className="dash-bgImg1" src={img3} alt="none" />
-          <img className="dash-bgImg2" src={img4} alt="none" />
-        </div>
-        <div style={{ marginTop: "100px" }}>
-          <div className="title-div-dashboard">
-            <div className="images-in-this">
-              {/* <img src={sameimg} alt="nonnn" /> */}
-            </div>
-            <h1>Effortless Token Distribution</h1>
-            <h3>
-              {" "}
-              Instant Multi-Account Dispersement – Seamlessly Send Tokens to
-              Multiple Accounts in One Click
-            </h3>
-          </div>
+
           <div className="main-div-for-all-option-dashboard">
             <div className="menu-bar-dashboard">
               <button
